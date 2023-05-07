134bcf270cf36f8ea1984dc73eb033d4c391638c
abandon888 <1876746719@qq.com>
2023-05-07 23:31:17 +0800

    feat:完成home基本页面

--- app/[lang]/home/loading.tsx
+++ app/[lang]/home/loading.tsx
@@ -3,7 +3,6 @@ export default function Loading() {
   return (
     <div className="space-y-4">
       <h1 className="text-xl font-medium text-gray-400/80">Loading...</h1>
-
       <div>
         <SkeletonCard isLoading={true} />
       </div>

810010fded1e62411ca76d638cc7719b4bfcfa07
abandon888 <1876746719@qq.com>
2023-05-08 22:37:56 +0800

    完成profile页面基本样式

---
+++ components/icons/campaignTwitter.tsx
@@ -0,0 +1,146 @@
+export const TwitterIcon = ({ ...props }) => {
+  return (
+    <svg
+      width="62"
+      height="63"
+      viewBox="0 0 62 63"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g filter="url(#filter0_d_1271_4433)">
+        <rect
+          x="11"
+          y="7"
+          width="40"
+          height="40"
+          rx="20"
+          fill="url(#paint0_linear_1271_4433)"
+          shapeRendering="crispEdges"
+        />
+        <rect
+          x="11.4"
+          y="7.4"
+          width="39.2"
+          height="39.2"
+          rx="19.6"
+          stroke="url(#paint1_radial_1271_4433)"
+          strokeWidth="0.8"
+          shapeRendering="crispEdges"
+        />
+        <rect
+          x="11.4"
+          y="7.4"
+          width="39.2"
+          height="39.2"
+          rx="19.6"
+          stroke="url(#paint2_radial_1271_4433)"
+          strokeWidth="0.8"
+          shapeRendering="crispEdges"
+        />
+      </g>
+      <g filter="url(#filter1_f_1271_4433)">
+        <path
+          d="M27.265 35.3869C35.3768 35.3869 39.8129 28.9346 39.8129 23.3394C39.8129 23.1563 39.8084 22.974 39.8003 22.7918C40.6634 22.1923 41.4082 21.45 42 20.5998C41.1953 20.9423 40.3415 21.1669 39.4672 21.266C40.3882 20.737 41.0776 19.9048 41.4071 18.9244C40.5413 19.4173 39.5941 19.7647 38.6065 19.9516C37.9421 19.2715 37.0624 18.8208 36.104 18.6694C35.1455 18.5181 34.1617 18.6746 33.3054 19.1147C32.449 19.5547 31.7679 20.2537 31.3678 21.1032C30.9676 21.9527 30.8707 22.9053 31.0921 23.813C29.3382 23.7288 27.6224 23.2915 26.0561 22.5294C24.4898 21.7673 23.108 20.6975 22.0005 19.3894C21.6083 20.0356 21.4019 20.7701 21.4022 21.5179C21.4022 22.9869 22.1814 24.2832 23.3655 25.0423C22.6651 25.0218 21.9802 24.8402 21.3682 24.5128L21.3673 24.5669C21.3675 25.5442 21.7199 26.4914 22.3647 27.2478C23.0095 28.0042 23.907 28.5234 24.9051 28.7172C24.2556 28.887 23.5742 28.9119 22.9132 28.7902C23.1948 29.6314 23.7433 30.3671 24.482 30.8944C25.2207 31.4217 26.1127 31.7142 27.0331 31.731C25.4712 32.9079 23.5421 33.5462 21.5563 33.5431C21.2043 33.5431 20.8532 33.5242 20.5039 33.4846C22.5202 34.7287 24.868 35.3891 27.265 35.3869Z"
+          fill="#1C92FF"
+        />
+      </g>
+      <path
+        d="M27.265 35.3869C35.3768 35.3869 39.8129 28.9346 39.8129 23.3394C39.8129 23.1563 39.8084 22.974 39.8003 22.7918C40.6634 22.1923 41.4082 21.45 42 20.5998C41.1953 20.9423 40.3415 21.1669 39.4672 21.266C40.3882 20.737 41.0776 19.9048 41.4071 18.9244C40.5413 19.4173 39.5941 19.7647 38.6065 19.9516C37.9421 19.2715 37.0624 18.8208 36.104 18.6694C35.1455 18.5181 34.1617 18.6746 33.3054 19.1147C32.449 19.5547 31.7679 20.2537 31.3678 21.1032C30.9676 21.9527 30.8707 22.9053 31.0921 23.813C29.3382 23.7288 27.6224 23.2915 26.0561 22.5294C24.4898 21.7673 23.108 20.6975 22.0005 19.3894C21.6083 20.0356 21.4019 20.7701 21.4022 21.5179C21.4022 22.9869 22.1814 24.2832 23.3655 25.0423C22.6651 25.0218 21.9802 24.8402 21.3682 24.5128L21.3673 24.5669C21.3675 25.5442 21.7199 26.4914 22.3647 27.2478C23.0095 28.0042 23.907 28.5234 24.9051 28.7172C24.2556 28.887 23.5742 28.9119 22.9132 28.7902C23.1948 29.6314 23.7433 30.3671 24.482 30.8944C25.2207 31.4217 26.1127 31.7142 27.0331 31.731C25.4712 32.9079 23.5421 33.5462 21.5563 33.5431C21.2043 33.5431 20.8532 33.5242 20.5039 33.4846C22.5202 34.7287 24.868 35.3891 27.265 35.3869Z"
+        fill="white"
+        style={{ mixBlendMode: 'overlay' }}
+      />
+      <defs>
+        <filter
+          id="filter0_d_1271_4433"
+          x="0.0487814"
+          y="0.429269"
+          width="61.9024"
+          height="61.9024"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4.38049" />
+          <feGaussianBlur stdDeviation="5.47561" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_1271_4433"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_1271_4433"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter1_f_1271_4433"
+          x="2.10673"
+          y="0.216108"
+          width="58.2904"
+          height="53.5678"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur
+            stdDeviation="9.19859"
+            result="effect1_foregroundBlur_1271_4433"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_1271_4433"
+          x1="14.5"
+          y1="5"
+          x2="33.3727"
+          y2="50.0774"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="white" stopOpacity="0.4" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
+        </linearGradient>
+        <radialGradient
+          id="paint1_radial_1271_4433"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(51.1659 45.5361) rotate(-140.546) scale(52.7391 36.183)"
+        >
+          <stop stopColor="#353C46" />
+          <stop offset="1" stopColor="#151515" stopOpacity="0" />
+        </radialGradient>
+        <radialGradient
+          id="paint2_radial_1271_4433"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(-7.44445 -1.83333) rotate(26.5462) scale(50.3447 43.1054)"
+        >
+          <stop stopColor="white" stopOpacity="0.24" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
+        </radialGradient>
+      </defs>
+    </svg>
+  );
+};

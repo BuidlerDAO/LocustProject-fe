fb185db5f9ae52b121d0152d48601cbca9e1f736
star <2654109566@qq.com>
2023-05-09 10:22:43 +0800

    fix: 更新 svg

--- components/icons/logoIcon.tsx
+++ components/icons/logoIcon.tsx
@@ -1,14 +1,14 @@
 const LogoIcon = () => {
   return (
     <svg
-      width="18"
-      height="14"
-      viewBox="0 0 18 14"
+      width="16"
+      height="15"
+      viewBox="0 0 16 15"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M-3.8147e-06 0V14C9.95675 14 18 7.71892 18 0H-3.8147e-06Z"
+        d="M0 0V15C8.29991 15 15.0047 8.27027 15.0047 0H0Z"
         fill="#6E62EE"
       />
     </svg>

1dd2745e374e9ec23835d0e790718311fc660fda
star <2654109566@qq.com>
2023-05-09 10:07:35 +0800

    feat: 删除 icon 图片,将其放置在独立组件

---
+++ components/icons/logoIcon.tsx
@@ -0,0 +1,17 @@
+const LogoIcon = () => {
+  return (
+    <svg
+      width="18"
+      height="14"
+      viewBox="0 0 18 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M-3.8147e-06 0V14C9.95675 14 18 7.71892 18 0H-3.8147e-06Z"
+        fill="#6E62EE"
+      />
+    </svg>
+  );
+};
+export default LogoIcon;
--- components/sider/sider.tsx
+++ components/sider/sider.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
 import { Layout, Menu } from 'antd';
 import {
   MenuFoldOutlined,
@@ -8,44 +8,46 @@ import {
   UserOutlined,
   VideoCameraOutlined
 } from '@ant-design/icons';
+import LogoIcon from '@/components/icons/logoIcon';
 
 export default function Sider() {
   const { Sider } = Layout;
 
-const handleMenuClick = (e) => {
+  const handleMenuClick = (e) => {
     const link = e.key;
     console.log(link);
   };
 
   return (
     <Sider trigger={null} className="h-screen">
-      <div className="m-4 h-8" >
-        <Image src="/assets/Logo.png" alt="logo" width={100} height={100} />
+      <div className="m-4 h-8">
+        <LogoIcon />
+        {/*<Image src="/assets/Logo.png" alt="logo" width={100} height={100} />*/}
         {/* <img src='../../assets/Logo.png' alt="logo" /> */}
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          onSelect={handleMenuClick}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: <Link href="/home/explore">Explore</Link>
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: <Link href="/home/post">new post</Link>
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: <Link href="/home/nav3">nav 3</Link>
-            }
-          ]}
-        />
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={['1']}
+        onSelect={handleMenuClick}
+        items={[
+          {
+            key: '1',
+            icon: <UserOutlined />,
+            label: <Link href="/home/explore">Explore</Link>
+          },
+          {
+            key: '2',
+            icon: <VideoCameraOutlined />,
+            label: <Link href="/home/post">new post</Link>
+          },
+          {
+            key: '3',
+            icon: <UploadOutlined />,
+            label: <Link href="/home/nav3">nav 3</Link>
+          }
+        ]}
+      />
     </Sider>
   );
 }

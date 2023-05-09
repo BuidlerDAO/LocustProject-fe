dae0d1ef9bfecaca3efea14571c8c1b38be63989
abandon888 <1876746719@qq.com>
2023-05-09 10:34:03 +0800

    factor:重构post页面

--- app/[lang]/home/post/page.tsx
+++ app/[lang]/home/post/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 import Link from 'next/link'
-import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select } from 'antd'
+import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select,Space,Upload,Input } from 'antd'
 import locale from 'antd/es/date-picker/locale/zh_CN'
+import { url } from 'inspector';
+import { PlusOutlined } from '@ant-design/icons'
 
 
 const { Option } = Select
@@ -9,61 +11,74 @@ const { RangePicker } = DatePicker
 
 const Post = () => {
   return (
-    <div>
+     <div className="publish">
       <Card
         title={
           <Breadcrumb separator=">">
             <Breadcrumb.Item>
               <Link href="/home">首页</Link>
             </Breadcrumb.Item>
-            <Breadcrumb.Item>内容管理</Breadcrumb.Item>
+            <Breadcrumb.Item>发布文章</Breadcrumb.Item>
           </Breadcrumb>
         }
-        style={{ marginBottom: 20 }}
       >
-        <Form initialValues={{ status: null }} onFinish={submitHandler}>
-          <Form.Item label="状态" name="status">
-            <Radio.Group>
-              <Radio value={null}>全部</Radio>
-              <Radio value={0}>草稿</Radio>
-              <Radio value={1}>待审核</Radio>
-              <Radio value={2}>审核通过</Radio>
-              <Radio value={3}>审核失败</Radio>
-            </Radio.Group>
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: 1 }}
+        >
+          <Form.Item
+            label="标题"
+            name="title"
+            rules={[{ required: true, message: '请输入文章标题' }]}
+          >
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
           </Form.Item>
-
-          <Form.Item label="频道" name="channel_id">
-            <Select
-              placeholder="请选择文章频道"
-              defaultValue="lucy"
-              style={{ width: 120 }}
-            >
-              <Option value="jack">Jack</Option>
-              <Option value="lucy">Lucy</Option>
+          <Form.Item
+            label="频道"
+            name="channel_id"
+            rules={[{ required: true, message: '请选择文章频道' }]}
+          >
+            <Select placeholder="请选择文章频道" style={{ width: 400 }}>
+              <Option value={0}>推荐</Option>
             </Select>
           </Form.Item>
 
-          <Form.Item label="日期" name="date">
-            <RangePicker locale={locale}></RangePicker>
+          <Form.Item label="封面">
+            <Form.Item name="type">
+              <Radio.Group>
+                <Radio value={1}>单图</Radio>
+                <Radio value={3}>三图</Radio>
+                <Radio value={0}>无图</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Upload
+              name="image"
+              listType="picture-card"
+              className="avatar-uploader"
+              showUploadList
+            >
+              <div style={{ marginTop: 8 }}>
+                <PlusOutlined />
+              </div>
+            </Upload>
           </Form.Item>
+          <Form.Item
+            label="内容"
+            name="content"
+            rules={[{ required: true, message: '请输入文章内容' }]}
+          ></Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit" style={{ marginLeft: 80 }}>
-              筛选
-            </Button>
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button size="large" type="primary" htmlType="submit">
+                发布文章
+              </Button>
+            </Space>
           </Form.Item>
         </Form>
       </Card>
     </div>
   )
-
-async function submitHandler() {
-  try {
-    // handle submit logic here
-  } catch (err) {
-    console.error(err);
-  }
-}
-
 }
 export default Post

b828a44e9760fd455396a90b65783e71ee924585
star <2654109566@qq.com>
2023-05-10 17:19:08 +0800

    fix: 修改 navbar 为相对定位

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -5,7 +5,9 @@ import LogoIcon from '@/components/icons/logoIcon';
 import { WalletConnect } from '@/components/wallet';
 const Navbar = () => {
   return (
-    <div className={`fixed z-50 flex h-[6rem] w-full flex-wrap items-center`}>
+    <div
+      className={`relative z-50 flex h-[6rem] w-full flex-wrap items-center bg-black`}
+    >
       <div className="sticky inset-0 z-10 flex h-full w-full max-w-full items-center px-8 py-2 lg:px-10 lg:py-4">
         <div className="text-blue-gray-900 dark:text-blue-gray-100 flex w-full items-center justify-between">
           <div className="flex items-center justify-center">

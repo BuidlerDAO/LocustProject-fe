f4f43235fa007b1be29e723950f7d97fb9ece157
star <2654109566@qq.com>
2023-05-10 18:28:41 +0800

    feat: 初步封装 avatar

---
+++ components/avatar/index.tsx
@@ -0,0 +1,40 @@
+import { FC, HTMLAttributes, memo } from 'react';
+import Image from 'next/image';
+import { AvatarIcon } from '@/components/icons/campaignAvatar';
+
+interface Props extends HTMLAttributes<HTMLElement> {
+  src?: string;
+  alt?: string;
+  width?: string;
+  height?: string;
+  className?: string;
+}
+
+const Avatar: FC<Props> = memo(
+  ({ width, height, src, alt, className, ...rest }: Props) => {
+    return (
+      <div
+        className={`flex items-center justify-center rounded-full border-4 border-[#191A27] bg-[#FFFFFF0D] outline outline-2 outline-[#FFFFFF33] ${
+          width ? width : 'w-24'
+        } ${height ? height : 'h-24'} ${className}`}
+        {...rest}
+      >
+        {src ? (
+          <Image
+            width={96}
+            height={96}
+            alt={alt as string}
+            src={src}
+            className="h-[100%] w-[100%] rounded-[50%]"
+          />
+        ) : (
+          <AvatarIcon />
+        )}
+      </div>
+    );
+  }
+);
+
+Avatar.displayName = 'Avatar';
+
+export default Avatar;

c0a52f77454d12c3ade17a054e87c25381ad684b
star <2654109566@qq.com>
2023-05-11 13:49:09 +0800

    feat: 增加路径判断显示不同 icon

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,9 +1,13 @@
 'use client';
 
 import { Typography } from '@/components/typography';
-import LogoIcon from '@/components/icons/logoIcon';
+import LogoIconTop from '@/components/icons/logoIconTop';
+import Logo from '@/components/icons/logo';
 import { WalletConnect } from '@/components/wallet';
+import { usePathname } from 'next/navigation';
 const Navbar = () => {
+  const path = usePathname();
+  const flag = path == '/zh-CN' || path == 'en';
   return (
     <div
       className={`relative z-50 flex h-[6rem] w-full flex-wrap items-center bg-black`}
@@ -11,8 +15,9 @@ const Navbar = () => {
       <div className="sticky inset-0 z-10 flex h-full w-full max-w-full items-center px-8 py-2 lg:px-10 lg:py-4">
         <div className="text-blue-gray-900 dark:text-blue-gray-100 flex w-full items-center justify-between">
           <div className="flex items-center justify-center">
+            {/*首页logo只有上半部分*/}
             <div className="mr-[-5px] mt-[-18px]">
-              <LogoIcon />
+              {flag ? <LogoIconTop /> : <Logo />}
             </div>
             <Typography
               variant="h1"

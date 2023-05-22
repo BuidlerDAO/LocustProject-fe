f9b3926d9829eee29ae34b49dde897e605a91e2b
star <2654109566@qq.com>
2023-05-22 23:09:37 +0800

    fix: 更改一些行内样式为小驼峰命名

--- components/icons/link.tsx
+++ components/icons/link.tsx
@@ -11,30 +11,30 @@ const LinkIcon = ({ ...props }) => {
       <path
         d="M13.12 8.18658L8.55003 3.61659C7.21938 2.28592 5.03263 2.31523 3.66577 3.68206C2.29893 5.04888 2.26962 7.23568 3.6003 8.56633L7.56793 12.534"
         stroke="#747474"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M16.4513 11.5015L20.419 15.4691C21.7497 16.7998 21.7203 18.9866 20.3535 20.3534C18.9867 21.7203 16.7999 21.7496 15.4692 20.4189L10.8992 15.8489"
         stroke="#747474"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M13.0546 13.0708C14.4215 11.704 14.4508 9.51722 13.1201 8.18652"
         stroke="#747474"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10.8994 10.8992C9.53257 12.266 9.50327 14.4528 10.8339 15.7834"
         stroke="#747474"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- components/icons/time.tsx
+++ components/icons/time.tsx
@@ -11,15 +11,15 @@ export const TimeIcon = ({ ...props }) => {
       <path
         d="M12 22C17.5229 22 22 17.5229 22 12C22 6.47715 17.5229 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5229 6.47715 22 12 22Z"
         stroke="#747474"
-        stroke-width="1.5"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
       />
       <path
         d="M12.0041 6L12.0035 12.0044L16.2432 16.2441"
         stroke="#747474"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );

14a68806f16f2de3a3f1aed6a9e4d1b68ce38163
abandon888 <1876746719@qq.com>
2023-05-23 21:50:10 +0800

    feat: 实现持久化存储

--- store/PostStore.tsx
+++ store/PostStore.tsx
@@ -21,32 +21,35 @@ type PostStore = {
   fetchPostsFromBackend: () => Promise<void>;
 };
 
-const usePostStore = create<PostStore>((set, get) => ({
-  // 从localStorage中获取数据
-  //posts: JSON.parse(localStorage.getItem('posts') || '{}'),
-  //count: parseInt(localStorage.getItem('count') || '0'),
-  posts: [],
-  setPosts: (posts) => set({ posts }),
-  // 设置数据的方法
-  increase: (post) => {
-    set(({ posts }) => ({ posts: [...posts, post] }));
-    //向后端发送数据
-    //apiPostData(post);
-  },
-  decrease: (id) =>
-    set(({ posts }) => ({ posts: posts.filter((p) => p.id !== id) })),
-  updatePost: (id, post) =>
-    set(({ posts }) => ({
-      posts: posts.map((p) => (p.id === id ? { ...p, ...post } : p))
-    })),
-  fetchPostsFromBackend: async () => {
-    try {
-      const response: AxiosResponse<Post[]> = await axios.get('/api/posts');
-      set({ posts: response.data });
-    } catch (error) {
-      console.error(error);
+const usePostStore = create(
+  persist(
+    (set, get) => ({
+      posts: [],
+      setPosts: (posts: Post[]) => set({ posts }),
+      increase: (post: Post) => {
+        set(({ posts }) => ({ posts: [...posts, post] }));
+        //向后端发送数据
+        //apiPostData(post);
+      },
+      decrease: (id: string) =>
+        set(({ posts }) => ({ posts: posts.filter((p) => p.id !== id) })),
+      updatePost: (id: string, post: Partial<Post>) =>
+        set(({ posts }) => ({
+          posts: posts.map((p) => (p.id === id ? { ...p, ...post } : p))
+        })),
+      fetchPostsFromBackend: async () => {
+        try {
+          const response: AxiosResponse<Post[]> = await axios.get('/api/posts');
+          set({ posts: response.data });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }),
+    {
+      name: 'post-storage' // name of the storage
     }
-  }
-}));
+  )
+);
 
 export default usePostStore;

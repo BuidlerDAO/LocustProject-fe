1de4073d766af9fb5a5cfb2c10e1f29acb16baf7
abandon888 <1876746719@qq.com>
2023-05-23 19:17:36 +0800

    fix: 去除ls报错

--- store/PostStore.tsx
+++ store/PostStore.tsx
@@ -24,8 +24,10 @@ type PostStore = {
 
 const usePostStore = create<PostStore>((set, get) => ({
   // 从localStorage中获取数据
-  posts: JSON.parse(localStorage.getItem('posts') || '{}'),
-  count: parseInt(localStorage.getItem('count') || '0'),
+  //posts: JSON.parse(localStorage.getItem('posts') || '{}'),
+  //count: parseInt(localStorage.getItem('count') || '0'),
+  posts: {},
+  count: 0,
   // 设置数据的方法
   increase: (post) => {
     const newCount = get().count + 1;
@@ -40,8 +42,8 @@ const usePostStore = create<PostStore>((set, get) => ({
       count: newCount
     });
     // 保存数据到localStorage并传输到后端
-    localStorage.setItem('posts', JSON.stringify(get().posts));
-    localStorage.setItem('count', newCount.toString());
+    //localStorage.setItem('posts', JSON.stringify(get().posts));
+    //localStorage.setItem('count', newCount.toString());
     apiPostData(get().posts);
   },
   decrease: (title: string) => {
@@ -52,8 +54,8 @@ const usePostStore = create<PostStore>((set, get) => ({
     // 更新posts和count
     set({ posts: newPosts, count: newPosts.length });
     // 保存数据到localStorage并传输到后端
-    localStorage.setItem('posts', JSON.stringify(newPosts));
-    localStorage.setItem('count', newPosts.length.toString());
+    //localStorage.setItem('posts', JSON.stringify(newPosts));
+    //localStorage.setItem('count', newPosts.length.toString());
     apiPostData(newPosts);
   }
 }));

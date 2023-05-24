77ddefeaf31ed9a80a73e49eff2761b2827a29ab
star <2654109566@qq.com>
2023-05-25 01:12:21 +0800

    feat: 增加 imgCrop 组件

---
+++ components/imgCrop/index.tsx
@@ -0,0 +1,50 @@
+import { FC, HTMLAttributes, memo, ReactNode } from 'react';
+import Cropper from 'react-cropper';
+
+interface ImgCropProps extends HTMLAttributes<HTMLElement> {
+  children?: ReactNode;
+  className?: string;
+  imgSrc?: string;
+  aspect?: number;
+  onChange?: (e: any) => void;
+  onCrop?: (e: any) => void;
+}
+
+const ImgCrop: FC<ImgCropProps> = memo((props: ImgCropProps) => {
+  // const getCropData = () => {
+  //   if (typeof cropper !== 'undefined') {
+  //     setCropData(cropper.getCroppedCanvas().toDataURL());
+  //     props.onCrop
+  //       ? props.onCrop(cropper.getCroppedCanvas().toDataURL())
+  //       : null;
+  //   }
+  // };
+
+  return (
+    <Cropper
+      className={`imgCrop w-[100%] ${props.className}`}
+      zoomTo={0.5}
+      zoomable={false} // 是否允许放大图像
+      preview=".img-preview"
+      src={props.imgSrc}
+      viewMode={1}
+      minCropBoxHeight={10}
+      minCropBoxWidth={10}
+      background={false} // 是否显示背景的马赛克
+      responsive={true}
+      autoCropArea={1}
+      rotatable={false}
+      aspectRatio={props.aspect || 1 / 1}
+      checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+      onInitialized={(instance: any) => {
+        props.onCrop ? props.onCrop(instance) : null;
+      }}
+      guides={true} // 显示在裁剪框上方的虚线
+      {...props}
+    />
+  );
+});
+
+ImgCrop.displayName = 'ImgCrop';
+
+export default ImgCrop;

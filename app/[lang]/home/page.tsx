4c8726d4fbef753008396741e48a654602a95bfc
abandon888 <1876746719@qq.com>
2023-05-26 21:01:15 +0800

    fix: 修复页面渲染问题

--- app/[lang]/home/page.tsx
+++ app/[lang]/home/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 'use client';
 import {
   DeleteOutlined,
@@ -11,23 +12,20 @@ import {
 import { Avatar, Button, Collapse, List, Space, Tooltip } from 'antd';
 import React, { useEffect, useState } from 'react';
 import Block from '@/components/blockCard/blockCard';
-import { usePostStore } from '@/store';
 import { apiGetPostList } from '@/apis/post';
-import { Post, PostData } from '@/store';
+import { Post } from '@/store';
 
 const App = () => {
   const [data, setData] = useState<Post[]>([]);
-  const onDelete = () => {
-    console.log('delete');
-  };
+
   useEffect(() => {
     getData();
   }, []);
+
   const getData = async () => {
     Promise.all([apiGetPostList({ offset: 0, limit: 10 })]).then(
       (values: any) => {
-        console.log(values[0].items[0]);
-        const arr = values.map((item: any) => ({
+        const newData = values.map((item: any) => ({
           title: item.items[0].title,
           link: item.items[0].link,
           originalText: item.items[0].body,
@@ -36,26 +34,26 @@ const App = () => {
           avatar: item.items[0].avatar,
           username: item.items[0].username
         }));
-        //console.log(postData);
-        console.log(arr);
-        setData(arr);
+        setData(newData);
       }
     );
-    return (
-      <div className="mr-16 mt-[100px]">
-        <List
-          itemLayout="vertical"
-          size="large"
-          style={{ color: 'white' }}
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item title={item.title}>
-              <Block data={item} />
-            </List.Item>
-          )}
-        />
-      </div>
-    );
   };
+
+  return (
+    <div className="mr-16 mt-[100px]">
+      <List
+        itemLayout="vertical"
+        size="large"
+        style={{ color: 'white' }}
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item title={item.title}>
+            <Block data={item} />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
 };
+
 export default App;

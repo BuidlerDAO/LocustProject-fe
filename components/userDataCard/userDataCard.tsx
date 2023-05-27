151a21ffdffb881d90d88d2dc22150498ad6aad8
abandon888 <1876746719@qq.com>
2023-05-27 19:46:40 +0800

    feat: data页面样式修改

--- components/userDataCard/userDataCard.tsx
+++ components/userDataCard/userDataCard.tsx
@@ -13,7 +13,7 @@ const UserDataCard = () => {
         <span className="absolute left-6 top-6 h-auto text-left font-semibold leading-5 text-[rgba(28,28,28,1)]">
           <span>Awarded</span>
         </span>
-        <span className="absolute left-6 top-[52px] h-auto text-left leading-9 text-[rgba(28,28,28,1)]">
+        <span className="absolute left-6 top-14 h-auto text-left leading-9 text-[rgba(28,28,28,1)]">
           <span className="text-2xl font-semibold">100 U</span>
         </span>
         <span className=" absolute right-[2vw] top-[22px] h-6 w-6">
@@ -25,7 +25,7 @@ const UserDataCard = () => {
         <span className="absolute left-6 top-6 h-auto text-left font-semibold leading-5 text-[rgba(28,28,28,1)]">
           <span>Points Earned</span>
         </span>
-        <span className="absolute left-6 top-[52px] h-auto text-left leading-9 text-[rgba(28,28,28,1)]">
+        <span className="absolute left-6 top-14 h-auto text-left leading-9 text-[rgba(28,28,28,1)]">
           <span className="text-2xl font-semibold">100</span>
         </span>
         <span className="absolute right-[2vw] top-[22px] h-6 w-6">
@@ -43,9 +43,9 @@ const UserDataCard = () => {
         <span className="absolute right-[2vw] top-[22px] h-6 w-6">
           <DollarCircleOutlined style={{ fontSize: '125%', color: 'black' }} />
         </span>
-        <div className="absolute right-[2vw] top-[60px] flex w-[81px] items-start gap-2 rounded-[44px] bg-[linear-gradient(_180deg,rgba(110,98,238,1)_2%,rgba(63,61,250,1)_100%_)] px-6 py-2">
+        <div className="absolute right-[2vw] top-14 flex w-[81px] items-start gap-2 rounded-[44px] bg-[linear-gradient(_180deg,rgba(110,98,238,1)_2%,rgba(63,61,250,1)_100%_)] px-6 py-2">
           <span className="text component3-text12 h-auto text-left text-xs font-medium leading-3 text-white no-underline">
-            <span>Claim</span>
+            <button>Claim</button>
           </span>
         </div>
       </div>

17741436c203e307aa3d54695c05df44fcc0fcce
star <2654109566@qq.com>
2023-05-30 19:12:50 +0800

    fix: 优化逻辑

--- components/signUp/index.tsx
+++ components/signUp/index.tsx
@@ -1,14 +1,13 @@
 import React, { memo } from 'react';
 import type { FC, ReactNode } from 'react';
 
-import { useUserStore } from '@/store';
-
 import { callContract } from '@/utils/callContract';
+import Link from 'next/link';
+
 interface MyProps {
   children?: ReactNode;
 }
 const SignUp: FC<MyProps> = () => {
-  const { isSignUp } = useUserStore();
   const contractAddress = '0xB8d30C0246d67Dc1aD419596f68d7a0cDad09060';
   return (
     <button onClick={() => callContract(contractAddress)}>

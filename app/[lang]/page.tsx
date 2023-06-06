1693788c1b72409f5901b5d1c8148710a869e7b2
abandon888 <1876746719@qq.com>
2023-06-06 14:49:35 +0800

    fix: 调节首页字体

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -186,7 +186,10 @@ const Index = memo((props: any) => {
               </div>
             </Button>
           </Link>
-          <span className="absolute right-[18.44rem] top-[16.69rem] break-words text-center text-[6.25rem] leading-[108%] text-white">
+          <span
+            className="absolute top-[16.69rem] break-words text-center text-[6.25rem] leading-[108%] text-white"
+            style={{ fontFamily: 'Poppins', fontWeight: '800' }}
+          >
             Cognitive <br />
             Locust Program
           </span>
@@ -215,11 +218,14 @@ const Index = memo((props: any) => {
           <div className="relative top-[13vw] flex flex-col items-center">
             <p
               className="text-[34px] font-semibold"
-              style={{ fontFamily: 'sans-serif' }}
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
             >
               Event Rules
             </p>
-            <p className="mt-[1vw] text-[18px] text-[#D36CFF] ">
+            <p
+              className="mt-[1vw] text-[18px] text-[#D36CFF] "
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
+            >
               由概要、内容要求、输出格式三部分
             </p>
           </div>
@@ -229,38 +235,63 @@ const Index = memo((props: any) => {
             </div>
             <Button
               className="relative ml-8 mt-8 box-border flex h-[2.38rem] cursor-default items-center whitespace-nowrap rounded-3xl px-6 text-xs font-medium leading-3"
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
               color="secondary"
             >
               Outline
             </Button>
-            <span className="relative ml-8 mt-7 h-6 max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-justify text-base font-medium leading-6 text-[#64c7ff]">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
+              className="relative ml-8 mt-7 h-6 max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-justify text-base font-medium leading-6 text-[#64c7ff]"
+            >
               1.Points
             </span>
-            <span className="relative ml-8 mt-1 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-1 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white"
+            >
               Each piece of content will be rewarded with 2 points (to be viewed
               when the BuidlerDAO economic model is released)
             </span>
-            <span className="relative ml-8 mt-3 h-6 max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-justify text-base font-medium leading-6 text-[#64c7ff]">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
+              className="relative ml-8 mt-3 h-6 max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-justify text-base font-medium leading-6 text-[#64c7ff]"
+            >
               2.Mechanism
             </span>
-            <span className="relative ml-8 mt-1 h-9 w-[69rem] overflow-hidden text-ellipsis whitespace-normal text-left text-xs leading-9 text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-1 h-9 w-[69rem] overflow-hidden text-ellipsis whitespace-normal text-left text-xs leading-9 text-white"
+            >
               Adopt betting + POW mining, locust members pledge 100 RMB per
               month, bet on 12 pieces of quality content to complete, monthly
               settlement punch card, the winner according to the POW division
               Pool.-Pool: The accumulated amount of all bets failed Locust &
               BuidlerDAO extra incentive 200 RMB/month
             </span>
-            <span className="relative ml-8 mt-[1.13rem] h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-[1.13rem] h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white"
+            >
               Bug 1:9 / Betting failure Deduct 100
             </span>
-            <span className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white"
+            >
               Bug 2:8 / Betting failure Deduct 100
             </span>
-            <span className="relative ml-8 mt-3 h-9 w-[18.75rem] overflow-hidden text-ellipsis whitespace-normal text-left text-xs text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-3 h-9 w-[18.75rem] overflow-hidden text-ellipsis whitespace-normal text-left text-xs text-white"
+            >
               Bug 3:16 / Betting success Retained 100.Additional 16-12=4;
               (4/6)*200=133.3 reward
             </span>
-            <span className="relative ml-8 mt-3 h-9 w-72 overflow-hidden text-ellipsis whitespace-normal text-left text-xs text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-3 h-9 w-72 overflow-hidden text-ellipsis whitespace-normal text-left text-xs text-white"
+            >
               Bug 4:14 / Betting success Keep 100, get 14-12=2 extra;
               (2/6)*200=66.7 reward
             </span>
@@ -272,26 +303,45 @@ const Index = memo((props: any) => {
             <Button
               className="relative ml-8 mt-8 box-border flex h-[2.38rem] cursor-default items-center whitespace-pre rounded-3xl px-6 text-xs font-medium leading-3"
               color="secondary"
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
             >
               Content requirements
             </Button>
-            <span className="relative ml-8 mt-7 h-6 max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-justify text-base font-medium leading-6 text-[#64c7ff]">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
+              className="relative ml-8 mt-7 h-6 max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-justify text-base font-medium leading-6 text-[#64c7ff]"
+            >
               1.Quantity
             </span>
-            <span className="relative ml-8 mt-1 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-1 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white"
+            >
               At least 3 articles per week and at least 12 articles per month;
             </span>
-            <span className="relative ml-8 mt-3 h-6 max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-justify text-base font-medium leading-6 text-[#64c7ff]">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
+              className="relative ml-8 mt-3 h-6 max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-justify text-base font-medium leading-6 text-[#64c7ff]"
+            >
               2.Quality
             </span>
-            <span className="mt-1; relative ml-8 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="mt-1; relative ml-8 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white"
+            >
               invalidate the number of consecutive posts at the end of the
               month; prohibit copy and paste without thinking;
             </span>
-            <span className="relative ml-8 mt-3 h-6 max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-justify text-base font-medium leading-6 text-[#64c7ff]">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
+              className="relative ml-8 mt-3 h-6 max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-justify text-base font-medium leading-6 text-[#64c7ff]"
+            >
               3.Audit
             </span>
-            <span className="relative ml-8 mt-1 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-1 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-pre text-left text-xs leading-[1.13rem] text-white"
+            >
               no complaints will be accepted for the time being if the content
               quality is not up to par/plagiarized;
             </span>
@@ -303,25 +353,44 @@ const Index = memo((props: any) => {
             <Button
               className="relative ml-8 mt-8 box-border flex h-[2.38rem] cursor-default items-center rounded-3xl px-6 text-xs font-medium leading-3"
               color="secondary"
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
             >
               Output format
             </Button>
-            <span className="relative ml-8 mt-7 h-6 max-w-[69rem] overflow-hidden text-ellipsis text-justify text-base font-medium leading-6 text-[#64c7ff]">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '500' }}
+              className="relative ml-8 mt-7 h-6 max-w-[69rem] overflow-hidden text-ellipsis text-justify text-base font-medium leading-6 text-[#64c7ff]"
+            >
               1.Output by
             </span>
-            <span className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-left text-xs text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis whitespace-nowrap text-left text-xs text-white"
+            >
               Subject
             </span>
-            <span className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis text-left text-xs text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis text-left text-xs text-white"
+            >
               Output by
             </span>
-            <span className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis text-left text-xs text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis text-left text-xs text-white"
+            >
               Core Content Introduction
             </span>
-            <span className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis text-left text-xs text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis text-left text-xs text-white"
+            >
               Personal Reflections
             </span>
-            <span className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis text-left text-xs text-white">
+            <span
+              style={{ fontFamily: 'Poppins', fontWeight: '400' }}
+              className="relative ml-8 mt-3 h-[1.13rem] max-w-[69rem] overflow-hidden text-ellipsis text-left text-xs text-white"
+            >
               Related Links
             </span>
           </div>

e442b63000ae2138639e54332a334cc8d86189cb
abandon888 <1876746719@qq.com>
2023-06-14 14:50:40 +0800

    chore: 删除部分未使用引入

--- app/[lang]/home/post/page.tsx
+++ app/[lang]/home/post/page.tsx
@@ -1,22 +1,4 @@
 'use client';
-import Link from 'next/link';
-import {
-  Breadcrumb,
-  Button,
-  Card,
-  DatePicker,
-  Form,
-  Input,
-  Radio,
-  Select,
-  Space,
-  Upload
-} from 'antd';
-import locale from 'antd/es/date-picker/locale/zh_CN';
-import { url } from 'inspector';
-import { PlusOutlined } from '@ant-design/icons';
-import { create } from 'zustand';
-import { usePostStore } from '@/store';
 import PostComponent from '@/components/postComponent/postComponent';
 
 const Post = () => {

9283e6a28bef66143cea63450570a42ff6618604
starr-starr <2654109566@qq.com>
2023-07-10 13:19:08 +0800

    fix: navbar will move when click connect button

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -73,8 +73,8 @@ const Navbar = () => {
       <div
         className={`absolute z-50 float-right flex h-[100px] flex-wrap items-center bg-[#04070B]
       ${
-        !flag && 'custom-width left-[18rem] border-b-[1px] border-b-lineGrey'
-      } ${flag && 'w-full'}`}
+        !flag && 'left-[18rem] w-[1209.13px] border-b-[1px] border-b-lineGrey'
+      } ${flag && 'w-[1519.2px]'}`}
       >
         <div className="sticky inset-0 z-10 flex h-full  max-w-full items-center px-8 py-2 lg:px-10 lg:py-4">
           <div className="text-blue-gray-900 dark:text-blue-gray-100 flex w-full items-center">

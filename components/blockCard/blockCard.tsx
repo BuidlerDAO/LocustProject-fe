835c5601eee44a7084d3e93b83ef1abb875b11d8
aboutmydreams <aboutmydreams@163.com>
2023-07-12 10:24:17 +0800

    fix: maxline number

--- components/blockCard/blockCard.tsx
+++ components/blockCard/blockCard.tsx
@@ -120,7 +120,7 @@ const Block = (props: { data: Post }) => {
               <span>Original Summary</span>
             </span>
             <span className="block-text08">
-              <TextMore text={props.data.originalText} maxLines={4} />
+              <TextMore text={props.data.originalText} maxLines={6} />
             </span>
           </div>
           <div className="block-line18" />
@@ -130,7 +130,7 @@ const Block = (props: { data: Post }) => {
             </span>
             <div className="block-group1">
               <span className="block-text12">
-                <TextMore text={props.data.personalThoughts} maxLines={6} />
+                <TextMore text={props.data.personalThoughts} maxLines={8} />
               </span>
             </div>
           </div>
